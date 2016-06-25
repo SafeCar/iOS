e7f1bb8177ce6da172daddd9feb7cdedf98a5795
rémi  <remirobert33530@gmail.com>
2016-06-26 02:38:11 +0800

    add web socket

--- CarEasyApp/FeatureViewModel.swift
+++ CarEasyApp/FeatureViewModel.swift
@@ -9,6 +9,7 @@
 import UIKit
 import RxSwift
 import RealmSwift
+import Starscream
 
 protocol FeatureViewModelDelegate {
     func showAddFeature()
@@ -19,6 +20,7 @@ class FeatureViewModel: FeatureViewModelProtocol {
     var models = Variable([FeatureCellViewModel]())
     var delegate: FeatureViewModelDelegate?
     private let disposeBag = DisposeBag()
+    private let socket = WebSocket(url: NSURL(string: "ws://192.168.128.245:4567/event")!)
     
     func addFeature() {
         self.delegate?.showAddFeature()
@@ -39,5 +41,45 @@ class FeatureViewModel: FeatureViewModelProtocol {
             guard let response = response else { return }
             print("response \(response)")
             }.addDisposableTo(self.disposeBag)
+    }
+    
+    init() {
+        self.socket.delegate = self
+        self.socket.connect()
+    }
+}
+
+extension FeatureViewModel: WebSocketDelegate {
+    func websocketDidConnect(socket: WebSocket) {
+        print("web socket connected")
+        let parameters = [
+            "driver_id": "remi",
+            "secret_id": "yo",
+            "filters": [
+                ["name": "seatbelt", "range": [0, 1, 2, 3]],
+                ["name": "inhibited", "range": ["airbag", "ESC"]],
+                ["name": "location", "range": [41.8383355, -88.2616092]],
+                ["name": "speed", "limit": 120],
+                ["name": "crash"]
+            ]
+        ]
+        
+        let data = try! NSJSONSerialization.dataWithJSONObject(parameters, options: NSJSONWritingOptions.PrettyPrinted)
+        let jsonString = NSString(data: data, encoding: NSUTF8StringEncoding)! as String
+        
+        print("JSON STRING : \(jsonString)")
+        socket.writeString(jsonString)
+    }
+    
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        print("web socket disconnected: \(error)")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        print("web socket get message : \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocket, data: NSData) {
+        print("web socket get data : \(data)")
     }
 }
