d38cd67b2329d3523bd39e1c9ab6dfbba15f8128
rémi  <remirobert33530@gmail.com>
2016-06-25 12:51:55 +0800

    add car manager

---
+++ CarEasyApp/CarManager.swift
@@ -0,0 +1,42 @@
+//
+//  CarManager.swift
+//  CarEasyApp
+//
+//  Created by Remi Robert on 25/06/16.
+//  Copyright © 2016 王笛iOS.Inc. All rights reserved.
+//
+
+import UIKit
+
+typealias SuccessBlock = ((value: [NSObject : AnyObject]?) -> Void)
+typealias SuccessMultipleBlock = ((value: [NSObject : AnyObject]?) -> Void)
+typealias ErrorBlock = ((error: NSError?) -> Void)
+
+class CarManager {
+    private var car: CEACoreCar
+    static var shareInstance = CarManager()
+    
+    static func getValue(key: String, success: SuccessBlock, error: ErrorBlock?) {
+        self.shareInstance.car.getValue(key, success: success, failure: error)
+    }
+    
+    static func getValues(keys: [String], success: SuccessBlock, error: ErrorBlock?) {
+        self.shareInstance.car.getValues(keys, success: success, failure: error)
+    }
+    
+    static func setValue(value: [NSObject : AnyObject], success: SuccessBlock, error: ErrorBlock?) {
+        self.shareInstance.car.setValue(value, success: success, failure: error)
+    }
+    
+    static func setValues(value: [NSObject : AnyObject], success: SuccessBlock, error: ErrorBlock?) {
+        self.shareInstance.car.setValues(value, success: success, failure: error)
+    }
+    
+    static func setTokenBasePath(tokenBaseUrlPath: String) {
+        self.shareInstance.car.setTokenBasePath(tokenBaseUrlPath)
+    }
+    
+    init() {
+        self.car = CEACoreCar()
+    }
+}
--- CarEasyApp/ViewController.swift
+++ CarEasyApp/ViewController.swift
@@ -22,14 +22,5 @@ class ViewController: UIViewController {
         }) { (error: NSError!) in
             NSLog("error")
         }
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    //*************************构建CEACoreCar*****************************
-    
-    
 }
