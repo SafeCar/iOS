8ae392db39dffe8cc2940258667e7c638940bf90
rémi  <remirobert33530@gmail.com>
2016-06-25 13:08:32 +0800

    get gps location, add init base path car manager

--- CarEasyApp/ViewController.swift
+++ CarEasyApp/ViewController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var car: CEACoreCar = CEACoreCar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //*************************构建CEACoreCar*****************************
-        car.setTokenBasePath("1-2")
-        car.getValue(CARSIGNALS_SPEED_CRUISE_CONTROL_MEM_1, success: { (dict: [NSObject : AnyObject]!) in
-            NSLog("%@", dict)
-        }) { (error: NSError!) in
-            NSLog("error")
+
+        CarManager.getValue(CARSIGNALS_SPEED_CRUISE_CONTROL_MEM_1, success: { (value) in
+            print("value : \(value)")
+            }) { (error) in
+                print("error : \(error)")
         }
     }
 }
